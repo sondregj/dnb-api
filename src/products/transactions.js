f605a048e79ed5a418e6ebeb1ae4439135799732
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2019-03-13 22:43:13 +0100

    :sparkles: Finished Transactions API

--- src/products/transactions.js
+++ src/products/transactions.js
@@ -8,10 +8,25 @@
 class Transactions {
 	constructor(client) {
 		this.client = client
+
+		this.basePath = '/transactions'
 	}
 
-	async getTransactions(account) {
-		
+	/**
+	 * Get transactions from an account
+	 * 
+	 * @param {String} accountNumber to check
+	 * @param {String} fromDate format 'YYYY-MM-DD'
+	 * @param {String} toDate format 'YYYY-MM-DD'
+	 */
+	async getTransactions(accountNumber, fromDate, toDate) {
+		const query = { fromDate, toDate }
+
+		try {
+			return await this.client.do.get(`${this.basePath}/${accountNumber}`, this.client.jwt, query)
+		} catch (err) {
+			throw err
+		}
 	}
 }
 
