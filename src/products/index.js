64f0a34145ffbccaf01bee3c9e82a398bcc791a5
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2019-05-11 01:47:46 +0200

    :fire: Removed API products that are no longer available

--- src/products/index.js
+++ src/products/index.js
@@ -1,11 +1,8 @@
 module.exports = {
 	API: require('./api'),
-	Accounts: require('./accounts'),
 	Cards: require('./cards'),
 	Currencies: require('./currencies'),
 	Customers: require('./customers'),
 	Locations: require('./locations'),
-	Payments: require('./payments'),
-	Transactions: require('./transactions'),
 	TestCustomers: require('./test-customers')
 }
